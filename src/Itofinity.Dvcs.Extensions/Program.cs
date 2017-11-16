076178f4ed20cc1bb08e945da1f8482a1c66fb78
Mike Minns <mminns@itofinity.co.uk>
2017-11-16 19:18:53 +0000

    Simplified back down to core program + appveyor definition

---
+++ src/Itofinity.Dvcs.Extensions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;   
+
+namespace Itofinity.Dvcs.Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Itofinity.Cli.Mef.Cli.Run(
+                args, 
+                typeof(Program).Assembly
+#if DEBUG
+                ,null
+                ,null
+                ,true
+#endif
+                );
+        }
+    }
+}
